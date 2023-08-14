e3fdf4031a273fbe5c2246f04596b94779ef4bf8
Artur Novikau <111690337+ArturNovikov@users.noreply.github.com>
2023-08-15 00:57:10 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,6 +4,21 @@ const alphabetContainer = document.getElementById('alphabet-container');
 const inputText = document.getElementById('input-text');
 const outputContainer = document.getElementById('output-container');
 
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function setRandomColors() {
+    alphabet.split('').forEach(letter => {
+        const colorInput = document.getElementById(letter);
+        colorInput.value = getRandomColor();
+    });
+}
 
 alphabet.split('').forEach((letter, index) => {
     const letterContainer = document.createElement('div');
